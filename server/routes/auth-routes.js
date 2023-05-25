7c925c4853ed512e288f4f3b65efdfdf23846e55
Ivan Sapego <sapego-ivan@list.ru>
2023-05-25 19:40:39 +0300

    first commit

--- server/routes/auth-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addUser, getUser } = require('../controllers/auth-controller');
-
-const router = express.Router();
-
-router.post('/user/add', addUser);
-
-module.exports = router;
